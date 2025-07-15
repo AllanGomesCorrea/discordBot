38cd5643cdbbb1534ba6de29d43d95205b1f466d
allan <allan.correa-ext@rtm.net.br>
2025-07-15 07:38:19 -0300

    Refactor command registration to replace 'fila' with 'queue' for consistency
    - Updated the command registration in __init__.py to use 'queue' instead of 'fila' for better alignment with English terminology.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,7 @@
 from .summary import summary
 from .total_splited import total_splited
 from .hello import hello
-from .play_song import add_song, play_pause, skip, fila, exit
+from .play_song import add_song, play_pause, skip, queue, exit
 
 def register_commands(bot):
     bot.tree.add_command(summary)
@@ -10,5 +10,5 @@ def register_commands(bot):
     bot.tree.add_command(add_song)
     bot.tree.add_command(play_pause)
     bot.tree.add_command(skip)
-    bot.tree.add_command(fila)
+    bot.tree.add_command(queue)
     bot.tree.add_command(exit) 
